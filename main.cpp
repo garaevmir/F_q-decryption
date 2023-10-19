6c7e4ed5ca4309f68c6470c9c7c1435b16bc8c17
garaevmir <garaevamir@gmail.com>
2023-10-19 14:18:49 +0300

    Mistakes correction

--- main.cpp
+++ main.cpp
@@ -1,153 +1,175 @@
-#include<iostream>
-#include<algorithm>
-#include<sstream>
+#include"main.h"
 
-std::vector<int64_t> poly;
-int64_t edge = 1e18;
-
-char number_to_char(uint64_t symbol) {
-    if (symbol <= 9)
-        return (char)(symbol + 48);
-    if (symbol <= 35)
-        return (char)(symbol + 55);
-    if (symbol <= 61)
-        return (char)(symbol + 61);
-    if (symbol == 62)
-        return 32;
-    if (symbol == 63)
-        return 46;
-    return '\n';
-}
-
-int64_t fast_degree(const int64_t& num, const int64_t& deg, const int64_t& module) {
-    if (deg == 0) {
-        return 1;
+namespace {
+    // Заданный алфавит
+    char number_to_char(const main_type& symbol) {
+        if (symbol <= 9)
+            return (char) (symbol + 48);
+        if (symbol <= 35)
+            return (char) (symbol + 55);
+        if (symbol <= 61)
+            return (char) (symbol + 61);
+        if (symbol == 62)
+            return 32;
+        if (symbol == 63)
+            return 46;
+        return '\n';
     }
-    if (deg % 2 == 1) {
-        return (fast_degree(num, deg - 1, module) * num) % module;
-    }
-    int64_t ans = fast_degree(num, deg >> 1,  module);
-    return (ans * ans) % module;
-}
-
-void numeral_system_plus(std::vector<int64_t>& number, int64_t& base, int64_t plus) {
-    std::vector<int64_t> mem;
-    int64_t temp = 0;
-    while (plus > 0) {
-        mem.push_back(plus % base);
-        plus = plus / base;
-    }
-    for (size_t i = 0; i < std::max(number.size(), mem.size()) || temp; ++i) {
-        if (i == number.size()) {
-            number.push_back(0);
+    // Алгоритм быстрого возведения в степень чисел
+    main_type fast_degree(const main_type& number, const main_type& degree, const main_type& mod) {
+        if (degree == 0) {
+            return 1;
         }
-        number[i] += temp + (i < mem.size() ? mem[i] : 0);
-        temp = number[i] >= base;
-        if (temp)  {
-            number[i] -= base;
+        if (degree % 2 == 1) {
+            return (fast_degree(number, degree - 1, mod) * number) % mod;
         }
+        main_type root = fast_degree(number, degree >> 1, mod);
+        return (root * root) % mod;
     }
-}
-
-void numeral_system_mult(std::vector<int64_t>& number, const int64_t& new_base, const int64_t base) {
-    int64_t temp = 0;
-    int64_t num;
-    for (size_t i = 0; i < number.size() || temp; ++i) {
-        if (i == number.size()) {
-            number.push_back(0);
+    // Сложение большого числа и малого числа в заданной системе счисления
+    void add_up_numeral_system(std::vector<main_type>& number, const main_type& base, main_type& plus) {
+        main_type temp = 0;
+        std::vector<main_type> memory;
+        while (plus > 0) {
+            memory.push_back(plus % base);
+            plus /= base;
         }
-        num = temp + number[i] * base;
-        temp = num / new_base;
-        number[i] = num - new_base * temp;
-    }
-}
-
-void string_to_polynom(std::string& input, std::vector<int64_t>& vec) {
-    std::stringstream in(input);
-    std::string num;
-    while (std::getline(in, num, ' ')) {
-        vec.push_back(std::stoi(num));
-    }
-}
-
-void poly_normalise(std::vector<int64_t>& num, const int64_t base) {
-    while (num.size() >= poly.size()) {
-        int64_t mult = num.back();
-        size_t n = num.size();
-        size_t m = poly.size();
-        for (size_t i = 1; i <= m; ++i) {
-            num[n - i] = (num[n - i] - poly[m - i] * mult) % base;
-            if (num[n - i] < 0) {
-                num[n - i] += base;
+        for (index_type i = 0; i < std::max(number.size(), memory.size()) || temp; ++i) {
+            if (i == number.size()) {
+                number.push_back(0);
+            }
+            number[i] += temp + (i < memory.size() ? memory[i] : 0);
+            temp = number[i] >= base;
+            if (temp) {
+                number[i] -= base;
             }
         }
-        while (num.size() > 1 && num.back() == 0) {
-            num.pop_back();
+    }
+    // Умножение большого числа на основание старой системы счисления
+    void multiply_numeral_system(std::vector<main_type>& number, const main_type& new_base, const main_type& base) {
+        main_type temp = 0;
+        main_type memory;
+        for (index_type i = 0; i < number.size() || temp; ++i) {
+            if (i == number.size()) {
+                number.push_back(0);
+            }
+            memory = temp + number[i] * base;
+            temp = memory / new_base;
+            number[i] = memory - new_base * temp;
         }
     }
-}
-
-std::vector<int64_t> poly_mult(std::vector<int64_t>& left, std::vector<int64_t>& right, const int64_t& base) {
-    std::vector<int64_t> result(left.size() + right.size(), 0);
-    for (int i = 0; i < left.size(); ++i) {
-        for (int j = 0; j < right.size(); ++j) {
-            result[i + j] = (result[i + j] + left[i] * right[j]) % base;
+    // Получение многочлена заданного строкой
+    std::vector<main_type> string_to_polynomial(std::string& input) {
+        std::stringstream text(input);
+        std::string number;
+        std::vector<main_type> output;
+        while (std::getline(text, number, ' ')) {
+            output.push_back(std::stoi(number));
+        }
+        return output;
+    }
+    // Нормализация многочлена, чтобы не было ни отрицательных членов, ни членов выше заданного mod
+    void normalise_polynomial(std::vector<main_type>& number, const main_type& base,
+                              const std::vector<main_type>& polynomial) {
+        while (number.size() >= polynomial.size()) {
+            main_type multiplier = number.back();
+            index_type number_size = number.size();
+            index_type polynomial_size = polynomial.size();
+            for (index_type i = 1; i <= polynomial_size; ++i) {
+                number[number_size - i] = (number[number_size - i] - polynomial[polynomial_size - i] * multiplier) % base;
+                if (number[number_size - i] < 0) {
+                    number[number_size - i] += base;
+                }
+            }
+            while (number.size() > 1 && number.back() == 0) {
+                number.pop_back();
+            }
         }
     }
-    while (result.size() > 1 && result.back() == 0) {
-        result.pop_back();
+    // Умножение двух многочленов
+    std::vector<main_type> multiply_polynomials(const std::vector<main_type>& left, const std::vector<main_type>& right,
+                                                const main_type& base, const std::vector<main_type>& polynomial) {
+        std::vector<main_type> result(left.size() + right.size());
+        for (index_type i = 0; i < left.size(); ++i) {
+            for (index_type j = 0; j < right.size(); ++j) {
+                result[i + j] = (result[i + j] + left[i] * right[j]) % base;
+            }
+        }
+        while (result.size() > 1 && result.back() == 0) {
+            result.pop_back();
+        }
+        normalise_polynomial(result, base, polynomial);
+        return result;
     }
-    poly_normalise(result, base);
-    return result;
-}
-
-std::vector<int64_t> poly_fast_degree(std::vector<int64_t>& num, const int64_t deg, const int64_t& module) {
-    if (deg == 0) {
-        std::vector<int64_t> temp(1, 1);
-        return temp;
+    // Алгоритм быстрого возведения многочлена в степень
+    std::vector<main_type> polynomial_fast_degree(const std::vector<main_type>& number, const main_type& degree,
+                                                  const main_type& mod, const std::vector<main_type>& polynomial) {
+        if (degree == 0) {
+            return {1};
+        }
+        if (degree % 2 == 1) {
+            std::vector<main_type> temp = polynomial_fast_degree(number, degree - 1, mod, polynomial);
+            return multiply_polynomials(temp, number, mod, polynomial);
+        }
+        std::vector<main_type> root = polynomial_fast_degree(number, degree >> 1, mod, polynomial);
+        return multiply_polynomials(root, root, mod, polynomial);
     }
-    if (deg % 2 == 1) {
-        std::vector<int64_t> mem = num;
-        std::vector<int64_t> temp = poly_fast_degree(num, deg - 1, module);
-        return poly_mult(temp, mem, module);
+    // Ввод шифра и его дешифровка в большое число в заданной системе счисления
+    std::vector<main_type> input_cipher(const std::vector<main_type>& polynomial, const main_type& mod,
+                                        const main_type& private_key) {
+        std::string input;
+        const main_type polynomial_size = (main_type)polynomial.size() - 1;
+        const main_type edge = 1e18;
+        const main_type ord = fast_degree(mod, polynomial_size, edge) - 2;
+        std::vector<main_type> message;
+        while (std::getline(std::cin, input, '\n') && !input.empty()) {
+            std::vector<main_type> input_1 = string_to_polynomial(input);
+            std::getline(std::cin, input, '\n');
+            std::vector<main_type> input_2 = string_to_polynomial(input);
+            input_1 = polynomial_fast_degree(input_1, private_key, mod, polynomial);
+            input_1 = polynomial_fast_degree(input_1, ord, mod, polynomial);
+            input_1 = multiply_polynomials(input_2, input_1, mod, polynomial);
+            while (input_1.size() < polynomial_size) {
+                input_1.push_back(0);
+            }
+            message.insert(message.end(), input_1.begin(), input_1.end());
+        }
+        return message;
     }
-    std::vector<int64_t> ans = poly_fast_degree(num, deg >> 1,  module);
-    return poly_mult(ans, ans, module);
+    // Перевод числа из одной системы счисления в другую
+    std::vector<main_type> translation_numeral_system(std::vector<main_type>& message, const main_type& base,
+                                                      const main_type& mod) {
+        std::vector<main_type> encrypted(1);
+        for (index_type i = message.size(); i >= 1; --i) {
+            multiply_numeral_system(encrypted, base, mod);
+            add_up_numeral_system(encrypted, base, message[i - 1]);
+        }
+        return encrypted;
+    }
+    // Вывод полученного сообщения
+    void output_message(const std::vector<main_type>& encrypted) {
+        for (main_type i : encrypted) {
+            std::cout << number_to_char(i);
+        }
+    }
 }
 
 int main() {
-    int64_t mod, base = 64;
-    int64_t private_key;
-    std::vector<int64_t> inp_1, inp_2;
+    // Задаём основание для системы счисления, соответствующей алфавиту из условия
+    const main_type base = 64;
+    // Приём соответственно модуля поля, неприводимого многочлена и приватного ключа
     std::string input;
     std::getline(std::cin, input);
-    mod = std::stoi(input);
+    const main_type mod = std::stoi(input);
     std::getline(std::cin, input);
-    string_to_polynom(input, poly);
+    std::vector<main_type> polynomial = string_to_polynomial(input);
     std::getline(std::cin, input);
-    private_key = std::stoi(input);
-    int64_t n = poly.size() - 1;
-    std::vector<int64_t> message;
-    while (std::getline(std::cin, input, '\n') && !input.empty()) {
-        string_to_polynom(input, inp_1);
-        std::getline(std::cin, input, '\n');
-        string_to_polynom(input, inp_2);
-        inp_1 = poly_fast_degree(inp_1, private_key, mod);
-        inp_1 = poly_fast_degree(inp_1, fast_degree(mod, n, edge) - 2, mod);
-        inp_1 = poly_mult(inp_2, inp_1, mod);
-        while (inp_1.size() < n) {
-            inp_1.push_back(0);
-        }
-        message.insert(message.end(), inp_1.begin(), inp_1.end());
-        inp_1.clear();
-        inp_2.clear();
-    }
-    std::vector<int64_t> encrypted(1, 0);
-    for (uint64_t i = message.size(); i >= 1; --i) {
-        numeral_system_mult(encrypted, base, mod);
-        numeral_system_plus(encrypted, base, message[i - 1]);
-    }
-    for (int64_t i : encrypted) {
-        std::cout << number_to_char(i);
-    }
+    const main_type private_key = std::stoi(input);
+    // Приём передаваемого шифра и расшифровка по схеме Эль-Гамаля
+    std::vector<main_type> message = input_cipher(polynomial, mod, private_key);
+    // Перевод полученного шифра из заданной системы счисления в систему счисления с имеющимся алфавитом
+    std::vector<main_type> encrypted = translation_numeral_system(message, base, mod);
+    // Вывод передаваемого сообщения
+    output_message(encrypted);
+    return 0;
 }
